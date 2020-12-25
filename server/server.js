1d3b8d09200ac50b6b5bbefacb540496dd86c5f4
Rob Mastronicola <arejaycola@gmail.com>
2020-12-25 17:09:35 -0500

    Added public route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
