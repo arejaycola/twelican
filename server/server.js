ece5edd77503574ef4fd4b4211570004b07a39cb
Rob Mastronicola <arejaycola@gmail.com>
2021-01-20 20:41:33 -0500

    Got basic user look up working with Twitter API example

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 const vhost = require('vhost');
