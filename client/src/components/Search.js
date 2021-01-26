a55650e9b6ebd77f28f55c2f38cd1442c53b17b4
Rob Mastronicola <arejaycola@gmail.com>
2021-01-25 19:51:44 -0500

    Make successful request to server from client event

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
 
 const Search = () => {
+	const [text, setText] = useState('');
+
+	const onFormSubmit = async (e) => {
+		e.preventDefault();
+
+		const response = await Axios.get('/api/hello');
+		console.log(response.data);
+	};
+
 	return (
-		<>
-			<input placeholder="Enter a verified Twitter Account" type="text" className="user-search" />
+		<form onSubmit={(e) => onFormSubmit(e)}>
+			<input
+				onChange={(e) => {
+					setText(e.target.value);
+				}}
+				value={text}
+				placeholder="Enter a verified Twitter Account"
+				type="text"
+				className="user-search"
+			/>
 			<button>Search</button>
-		</>
+		</form>
 	);
 };
 export default Search;
