087e553702b653d5b7b3f1a3203cd03a5e739b10
Rob Mastronicola <arejaycola@gmail.com>
2021-01-27 20:10:57 -0500

    Search results are now displayed in a basic list below the search box

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -3,28 +3,38 @@ import Axios from 'axios';
 
 const Search = () => {
 	const [searchText, setSearchText] = useState('');
+	const [searchResults, setSearchResults] = useState([]);
 
 	const onFormSubmit = async (e) => {
 		e.preventDefault();
 
 		/* Just search Rob if no text is inputted. */
 		const response = await Axios.get(`/search/${searchText ? searchText : 'Rob'}`);
-		console.log(response.data);
+		if (response) {
+			setSearchResults(response.data);
+		}
 	};
 
 	return (
-		<form onSubmit={(e) => onFormSubmit(e)}>
-			<input
-				onChange={(e) => {
-					setSearchText(e.target.value);
-				}}
-				value={searchText}
-				placeholder="Enter a verified Twitter Account"
-				type="text"
-				className="user-search"
-			/>
-			<button>Search</button>
-		</form>
+		<>
+			<form onSubmit={(e) => onFormSubmit(e)}>
+				<input
+					onChange={(e) => {
+						setSearchText(e.target.value);
+					}}
+					value={searchText}
+					placeholder="Enter a verified Twitter Account"
+					type="text"
+					className="user-search"
+				/>
+				<button>Search</button>
+			</form>
+			<ul>
+				{searchResults.map((result) => {
+					return <li key={result.id}>{result.name}</li>;
+				})}
+			</ul>
+		</>
 	);
 };
 export default Search;
