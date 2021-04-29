ebd02e60bfb05ff68b122028508464925f78fad8
Rob Mastronicola <arejaycola@gmail.com>
2021-04-28 22:12:26 -0400

    Got context set up. Trying to figure out layout and architecture.

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Axios from 'axios';
+import { SearchContext } from '../context/SearchContext';
 
 const Search = () => {
 	const [searchText, setSearchText] = useState('');
-	const [searchResults, setSearchResults] = useState([]);
+	const { setSearchResults } = useContext(SearchContext);
 
 	const onFormSubmit = async (e) => {
 		e.preventDefault();
 
+		/* TODO (04/28/2021 22:11) this is not working */
 		/* Just search Rob if no text is inputted. */
 		const response = await Axios.get(`/search/${searchText ? searchText : 'Rob'}`);
 		if (response) {
+			console.log(response.data);
 			setSearchResults(response.data);
 		}
 	};
@@ -29,11 +32,6 @@ const Search = () => {
 				/>
 				<button>Search</button>
 			</form>
-			<ul>
-				{searchResults.map((result) => {
-					return <li key={result.id}>{result.name}</li>;
-				})}
-			</ul>
 		</>
 	);
 };
